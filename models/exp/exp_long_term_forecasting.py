b460957087f87a6af8c34d440548c181b2b8f7bb
jw-251 <119660463+EyesDawn@users.noreply.github.com>
2025-12-12 12:42:44 +0000

    fix: result output without using os.path.join()

--- models/exp/exp_long_term_forecasting.py
+++ models/exp/exp_long_term_forecasting.py
@@ -283,13 +283,10 @@ class Exp_Long_Term_Forecast(Exp_Basic):
         mae, mse, rmse, mape, mspe = metric(preds, trues)
         # print('mse:{}, mae:{}, dtw:{}'.format(mse, mae, dtw))
         print('mse:{}, mae:{}'.format(mse, mae))
-        f = open("result_long_term_forecast.txt", 'a')
-        f.write(setting + "  \n")
-        # f.write('mse:{}, mae:{}, dtw:{}'.format(mse, mae, dtw))
-        f.write('mse:{}, mae:{}'.format(mse, mae))
-        f.write('\n')
-        f.write('\n')
-        f.close()
+        with open(os.path.join(folder_path, 'result_long_term_forecast.txt'), 'a') as f:
+            f.write(setting + '\n')
+            f.write('mse:{}, mae:{}\n'.format(mse, mae))
+            f.write('\n')
 
         if save_result:
             # np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
